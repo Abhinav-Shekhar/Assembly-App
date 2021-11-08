d0016eee3bf7498a27b587523f243f35d1513df4
Abhinav-Shekhar <abhirocksnav@gmail.com>
2021-11-08 14:58:43 +0530

    added start button

---
+++ app/src/main/java/com/android/task/Cart.java
@@ -0,0 +1,4 @@
+package com.android.task;
+
+public class Cart {
+}
--- app/src/main/java/com/android/task/MainActivity.java
+++ app/src/main/java/com/android/task/MainActivity.java
@@ -1,14 +1,38 @@
 package com.android.task;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.annotation.RequiresApi;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.parseColor("#000000"));
+
+        Button buttonStart = (Button) findViewById(R.id.start);
+        buttonStart.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Cart.class);
+            startActivity(intent);
+        });
     }
 }
